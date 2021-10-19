33a4732e6f017c26a333b251fdc5f14ff11a5d51
Sam Holleworth <s_holleworth@outlook.com>
2021-10-19 16:23:10 +0100

    feat(Chart): began building static version of the main chart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import React, { useEffect, useState } from "react"
+=======
+import React from "react"
+>>>>>>> 24253b5 (feat(Chart): began building static version of the main chart)
 import {
   LineChart,
   Line,
@@ -7,6 +11,7 @@ import {
   CartesianGrid,
   ReferenceArea,
   ResponsiveContainer,
+<<<<<<< HEAD
   Text,
 } from "recharts"
 import { requestIntradayPrices } from "./services"
@@ -89,6 +94,37 @@ export const Chart = ({ data, interval }) => {
     <Line
       type="linear"
       dataKey="average"
+=======
+} from "recharts"
+
+const ChartContainer = () => {
+  let data = [
+    { time: Date.now(), price: 318 },
+    { time: Date.now() + 60000, price: 318 },
+    { time: Date.now() + 120000, price: 408 },
+    { time: Date.now() + 180000, price: 388 },
+    { time: Date.now() + 240000, price: 178 },
+    { time: Date.now() + 300000, price: 233 },
+  ]
+
+  // data = data.sort((a, b) => a.pv - b.pv)
+
+  return <Chart data={data} />
+}
+
+export const Chart = ({ data }) => {
+  const colours = {
+    accentPrimary: "#aaabd1",
+    coreSecondary2: "#f1f2f2",
+    coreSecondary3: "#eaebeb",
+    keys: "#7f7f7f",
+  }
+
+  const line = (
+    <Line
+      type="linear"
+      dataKey="price"
+>>>>>>> 24253b5 (feat(Chart): began building static version of the main chart)
       dot={false}
       stroke={colours.accentPrimary}
       strokeWidth={2}
@@ -96,6 +132,7 @@ export const Chart = ({ data, interval }) => {
   )
 
   const grid = (
+<<<<<<< HEAD
     <CartesianGrid
       stroke={colours.coreSecondary3}
       vertical={false}
@@ -175,13 +212,58 @@ export const Chart = ({ data, interval }) => {
         {referenceAreas}
         {grid}
         {line}
+=======
+    <CartesianGrid stroke={colours.coreSecondary3} vertical={false} />
+  )
+
+  const xAxis = (
+    <XAxis
+      type="number"
+      dataKey="time"
+      domain={[data[0].time, data[data.length - 1].time]}
+      axisLine={false}
+      tickCount={6}
+      tickSize={12}
+      tickLine={{ stroke: colours.coreSecondary3 }}
+      stroke={colours.keys}
+    />
+  )
+
+  const yAxis = (
+    <YAxis
+      type="number"
+      dataKey="price"
+      axisLine={false}
+      tickSize={12}
+      tickLine={{ stroke: colours.coreSecondary3 }}
+      stroke={colours.keys}
+    />
+  )
+
+  const renderChart = (
+    <ResponsiveContainer width="100%" height={600}>
+      <LineChart width={400} height={400} data={data}>
+        <ReferenceArea
+          x1={Date.now()}
+          x2={Date.now() + 60000}
+          y1={0}
+          y2={600}
+          fill={colours.coreSecondary2}
+        />
+        {line}
+        {grid}
+>>>>>>> 24253b5 (feat(Chart): began building static version of the main chart)
         {xAxis}
         {yAxis}
       </LineChart>
     </ResponsiveContainer>
   )
 
+<<<<<<< HEAD
   return renderChart
+=======
+  return <div>{renderChart}</div>
+>>>>>>> 24253b5 (feat(Chart): began building static version of the main chart)
 }
 
 export default ChartContainer
