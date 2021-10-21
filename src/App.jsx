0e6b87b24bb575910157e7cafb64684bf8c687f8
Sam <s_holleworth@outlook.com>
2021-10-21 15:10:45 +0100

    feat(NewsFeed): added news feed column, hooked up to IEX Cloud news endpoint and styled

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Stats from "./components/KeyStatistics/Stats"
 import KeyStatistics from "./components/KeyStatistics"
 import Chart from "./components/Chart"
 import "./App.css"
+import NewsFeed from "./components/NewsFeed/NewsFeed"
 
 function App() {
   return (
@@ -11,6 +12,13 @@ function App() {
       <KeyStatistics />
       <Chart />
       {/* <KeyStatistics /> */}
+      <div className="main-section">
+        <Chart />
+        <KeyStatistics />
+      </div>
+      <div className="news-summary-section">
+        <NewsFeed />
+      </div>
     </div>
   )
 }
