dc58baf60d45a530ca18f5e4b92e5f59eb66d703
Ricardo Delgado <ricardojdg13@gmailcom>
2021-10-26 14:24:24 +0200

    feat: added svg arrow and colors to price changes

--- src/components/LivePrice/LivePrice.jsx
+++ src/components/LivePrice/LivePrice.jsx
@@ -1,12 +1,31 @@
 import React from "react"
 import { useSymbol } from "../../contexts/SymbolContext"
 import useLivePrice from "./useLivePrice"
+import "./styles.css"
+import downArrow from "../../assets/redarrow.svg"
+import normalArrow from "../../assets/normal.svg"
 
 const LivePrice = () => {
   const { symbol } = useSymbol()
   const price = useLivePrice(symbol)
 
-  return <div>{price?.[0].latestPrice}</div>
+  return (
+    <div className="price__display">
+      <span className="price">{`$${price?.[0].latestPrice.toFixed(2)}`}</span>
+      <img
+        className="arrow"
+        src={price?.[0].change > 0 ? normalArrow : downArrow}
+        alt="down arrow"
+      />
+      <span
+        className={
+          price?.[0].change > 0 ? "change positive" : "change negative"
+        }
+      >
+        {`${price?.[0].change} | ${price?.[0].changePercent.toFixed(2)}%`}
+      </span>
+    </div>
+  )
 }
 
 export default LivePrice
--- src/components/LivePrice/useLivePrice.jsx
+++ src/components/LivePrice/useLivePrice.jsx
@@ -2,9 +2,6 @@ import { useEffect, useState } from "react"
 
 const useLivePrice = (symbol) => {
   const [price, setPrice] = useState()
-  /*   const BASE_URL = `https://sandbox-sse.iexapis.com/stable/stock/${symbol}/quote/latestPrice?token=${
-    import.meta.env.VITE_IEX_TOKEN
-  }` */
   const CURL_URL = `https://sandbox-sse.iexapis.com/stable/stocksUS?symbols=${symbol}&token=${
     import.meta.env.VITE_IEX_TOKEN
   }`
@@ -32,3 +29,64 @@ const useLivePrice = (symbol) => {
 }
 
 export default useLivePrice
+/*
+[
+  {
+    avgTotalVolume: 7486593,
+    calculationPrice: "close",
+    change: 0.31,
+    changePercent: 0.00245,
+    close: 136.66,
+    closeSource: "iofcifla",
+    closeTime: 1662749987289,
+    companyName: "Qualcomm, Inc.",
+    currency: "USD",
+    delayedPrice: 137.45,
+    delayedPriceTime: 1657945668940,
+    extendedChange: 0.76,
+    extendedChangePercent: 0.00568,
+    extendedPrice: 136.79,
+    extendedPriceTime: 1687354891027,
+    high: 138.59,
+    highSource: " erctp i5uedlm1dieya en",
+    highTime: 1689222840409,
+    iexAskPrice: 0,
+    iexAskSize: 0,
+    iexBidPrice: 0,
+    iexBidSize: 0,
+    iexClose: 132.91,
+    iexCloseTime: 1652986007577,
+    iexLastUpdated: 0,
+    iexMarketPercent: null,
+    iexOpen: 134.95,
+    iexOpenTime: 1707036259757,
+    iexRealtimePrice: 0,
+    iexRealtimeSize: 0,
+    iexVolume: 0,
+    lastTradeTime: 1711813797311,
+    latestPrice: 137.44,
+    latestSource: "Close",
+    latestTime: "October 25, 2021",
+    latestUpdate: 1690847603494,
+    latestVolume: 4153526,
+    low: 132.4,
+    lowSource: " tu1e epnmyddleire5cai ",
+    lowTime: 1657928338598,
+    marketCap: 150762300843,
+    oddLotDelayedPrice: 134.11,
+    oddLotDelayedPriceTime: 1665953568347,
+    open: 134,
+    openTime: 1658400806094,
+    openSource: "flcofaii",
+    peRatio: 16.63,
+    previousClose: 134.5,
+    previousVolume: 5292513,
+    primaryExchange: "ASNDQA",
+    symbol: "QCOM",
+    volume: 0,
+    week52High: 168.39,
+    week52Low: 119.3,
+    ytdChange: -0.1224420774451382,
+  }
+]
+ */
