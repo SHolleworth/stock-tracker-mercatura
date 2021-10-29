aaf2a17c13022abbaf3bd95f250677e50ebbf8fc
Sam <s_holleworth@outlook.com>
2021-10-29 13:28:19 +0100

    refactor: changed magic numbers in convertMillisecondsToNewsFeedTime to enums

--- src/utils/convertMillisecondsToNewsFeedTime.test.js
+++ src/utils/convertMillisecondsToNewsFeedTime.test.js
@@ -1,13 +1,5 @@
 import convertMillisecondsToNewsFeedTime from "./convertMillisecondsToNewsFeedTime"
-
-const millisecondTimes = {
-	second: 1000,
-	minute: 60000,
-	hour: 3.6e6,
-	day: 8.64e7,
-	month: 2.628e9,
-	year: 3.154e10,
-}
+import { millisecondTimes } from "./convertMillisecondsToNewsFeedTime"
 
 it("converts to 'Now'", () => {
 	expect(convertMillisecondsToNewsFeedTime(Date.now() - 110)).toBe("Now")
