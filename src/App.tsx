dad4e7c14f526f9ba0d52dbb0b12d0957b78854b
Sam <s_holleworth@outlook.com>
2021-11-18 11:40:45 +0000

    fix(routing): app loads base url only is history.length = 0, to fix refreshes

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,20 @@ import SplashScreen from "./screens/SplashScreen"
 import StockScreen from "./screens/StockScreen/StockScreen"
 import { Switch, Route, useHistory } from "react-router-dom"
 import ROUTES from "./utils/routes"
+import { useSymbol } from "./contexts/SymbolContext"
 
 function App() {
 	const history = useHistory()
+	const { symbol } = useSymbol()
+
+	console.log("SYMBOL: " + symbol)
 
 	useEffect(() => {
-		history.push(ROUTES.base)
+		console.log("AFTER MOUNTING APP")
+		if (history.length < 1) {
+			history.push(ROUTES.base)
+		}
+		return () => console.log("UNMOUNTING APP")
 	}, [])
 
 	return (
@@ -17,7 +25,7 @@ function App() {
 				<Route path={`${ROUTES.base}stock/:stock`}>
 					<StockScreen />
 				</Route>
-				<Route exact path={ROUTES.base}>
+				<Route path={ROUTES.base}>
 					<SplashScreen />
 				</Route>
 			</Switch>
