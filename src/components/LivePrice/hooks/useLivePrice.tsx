88f0182ced71ed26fde591f4e6cba3709fb91594
RicardoJDG <ricardojdg13@gmail.com>
2021-11-24 12:09:49 +0100

    refactor: implemented rxjs for liveprices

--- src/components/LivePrice/hooks/useLivePrice.tsx
+++ src/components/LivePrice/hooks/useLivePrice.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react"
-import { requestLatestPrice } from "../services"
+// import { requestLatestPrice } from "../services"
 import STATUS from "../../../utils/statusKeys"
 import { Price } from "../types"
 import { base_sse as base } from "../../../utils/baseUrl"
+import { Observable } from "rxjs"
+import { map, filter, catchError } from "rxjs/operators"
 // import { tokens } from "../../../../../token"
 
 //Map whole body response object for the type
@@ -18,40 +20,41 @@ const useLivePrice = (symbol: string, updateInterval: 1 | 5) => {
 	}`
 	// const CURL_URL = `${base}stocksUS${updateInterval}Second?symbols=${symbol}&token=${tokens.REAL_TOKEN}`
 
+	function fromEventSource(url: string): Observable<MessageEvent> {
+		return new Observable<MessageEvent>((subscriber) => {
+			const sse = new EventSource(url)
+
+			sse.onmessage = (e) => subscriber.next(e)
+			sse.onerror = (e) => subscriber.error(e)
+
+			return () => {
+				if (sse.readyState === 1) {
+					sse.close()
+				}
+			}
+		})
+	}
+
 	useEffect(() => {
 		setPrice({ status: STATUS.LOADING })
 
-		const sse = new EventSource(CURL_URL)
-
-		sse.onopen = () => {
-			console.log("SSE connection established")
-		}
-
-		sse.onmessage = (e) => {
-			const data = JSON.parse(e.data)
-			if (data.length !== 0) {
+		const subscription = fromEventSource(CURL_URL)
+			.pipe(
+				map((message) => JSON.parse(message.data)[0]),
+				filter((data) => data),
+				catchError((err, catched) => {
+					console.log(err)
+					return catched
+				})
+			)
+			.subscribe((data) =>
 				setPrice({
 					status: STATUS.RESOLVED,
-					body: JSON.parse(e.data)[0],
+					body: data,
 				})
-			} else {
-				console.log("Just got an empty message")
-			}
-		}
+			)
 
-		sse.onerror = async (error) => {
-			console.log(error)
-			sse.close()
-			try {
-				const latestPrice = await requestLatestPrice(symbol)
-				setPrice({ status: STATUS.RESOLVED, body: latestPrice })
-			} catch (error) {
-				console.error(error)
-				setPrice({ status: STATUS.ERROR })
-			}
-		}
-
-		return () => sse.close()
+		return () => subscription.unsubscribe()
 	}, [CURL_URL, symbol])
 
 	return price
